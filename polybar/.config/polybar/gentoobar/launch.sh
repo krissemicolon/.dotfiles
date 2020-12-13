dd9ba697bba5d348f0af0fc6f0e9d406b52c181d
Lian Studer <lianstuder12@gmail.com>
2020-12-13 18:30:59 +0100

    ubdate

--- polybar/.config/polybar/gentoobar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-# Gentoobar launcher
-
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar main -c ~/.config/polybar/gentoobar/config.ini &
-
